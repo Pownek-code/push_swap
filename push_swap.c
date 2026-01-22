7e7cc9a1d0642a1a1c5cdc572685f8dbc474077d
Pownek-code <lizzotuco0@gmail.com>
2026-01-22 15:30:30 -0800

    fixed norminette.

--- push_swap.c
+++ push_swap.c
@@ -11,24 +11,18 @@
 /* ************************************************************************** */
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+static char	**parse_arguments(int argc, char **argv)
 {
-	t_stack_node	*a;
-	t_stack_node	*b;
-	char			*full_arg_str;
-	char			**split_args;
-	int			i;
+	char	*full_arg_str;
+	char	**split_args;
+	int		i;
+	char	*temp;
 
-	a = NULL;
-	b = NULL;
 	full_arg_str = NULL;
-	split_args = NULL;
-	if (argc == 1 || (argc == 2 && !argv[1][0]))
-		return (1);
 	i = 1;
 	while (i < argc)
 	{
-		char *temp = full_arg_str;
+		temp = full_arg_str;
 		full_arg_str = ft_strjoin(temp, argv[i]);
 		if (temp)
 			free(temp);
@@ -41,17 +35,37 @@ int	main(int argc, char **argv)
 	}
 	split_args = split(full_arg_str, ' ');
 	free(full_arg_str);
+	return (split_args);
+}
+
+static void	perform_sort(t_stack_node **a, t_stack_node **b)
+{
+	if (!stack_sorted(*a))
+	{
+		if (stack_len(*a) == 2)
+			sa(a, false);
+		else if (stack_len(*a) == 3)
+			tiny_sort(a);
+		else
+			sort_stacks(a, b);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack_node	*a;
+	t_stack_node	*b;
+	char			**split_args;
+
+	a = NULL;
+	b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	split_args = parse_arguments(argc, argv);
 	init_stack_a(&a, split_args + 1);
 	free_argv(split_args);
-	if (!stack_sorted(a))
-	{
-		if (stack_len(a) == 2)
-			sa(&a, false);
-		else if (stack_len(a) == 3)
-			tiny_sort(&a);
-		else
-			sort_stacks(&a, &b);
-	}
+	perform_sort(&a, &b);
 	free_stack(&a);
 	return (0);
 }
+
